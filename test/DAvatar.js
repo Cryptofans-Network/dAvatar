3d852898d224763eda539c8732ce3a0e4bb92a19
Petar Popovic <petarpnp@gmail.com>
2021-08-27 10:44:53 +0200

    Add basic mint implementation

--- test/DAvatar.js
+++ test/DAvatar.js
@@ -15,4 +15,31 @@ describe("DAvatar", function () {
     expect(await dAvatar.name()).to.equal("dAvatar");
     expect(await dAvatar.symbol()).to.equal("dA");
   });
+
+  it("Should mint new NFT token with correct metadata and assign to correct owner", async function () {
+    const [defaultAccount, account1] = await ethers.getSigners();
+
+    const mintTx = await dAvatar.mint(defaultAccount.address, "testMetadaCid");
+    await mintTx.wait();
+
+    expect(await dAvatar.ownerOf(1)).to.equal(defaultAccount.address);
+
+    // @dev From should be zero when minting
+    //
+    // See {ERC721-_beforeTokenTransfer}.
+    //
+    await expect(dAvatar.mint(account1.address, "testMetadaCid"))
+      .to.emit(dAvatar, "Transfer")
+      .withArgs(ethers.constants.AddressZero, account1.address, 2);
+
+    expect(await dAvatar.ownerOf(2)).to.equal(account1.address);
+  });
+
+  it("Should revert if mint is called by caller that doesn't have MINT_ROLE", async function () {
+    const [defaultAccount, account1] = await ethers.getSigners();
+
+    await expect(
+      dAvatar.connect(account1).mint(defaultAccount.address, "testMetadaCid")
+    ).to.be.revertedWith("ERC721Base: must have minter role to mint");
+  });
 });
